d3ac6c62425463f5742cd546b22bf4f4f42ddc51
JohnQuimson <john.quimson777@gmail.com>
2024-05-28 14:40:18 +0200

    feat: middleware files

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const postsRouter = require('./routers/posts.js');
 
-//middelware generici
+const postsRouter = require('./routers/posts.js');
+const routersLogger = require('./middlewares/routersLogger.js');
+const errorsFormatter = require('./middlewares/errorsFormatter.js');
+const routesNotFound = require('./middlewares/routesNotFound.js');
+
 app.use(express.static('./public'));
+
+app.use(routersLogger);
 
 // routers
 app.get('/', (req, res) => {
@@ -12,6 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/posts', postsRouter);
+app.use(routesNotFound);
+app.use(errorsFormatter);
 
 //start server
 app.listen(3000, () => {
